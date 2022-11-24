52dd0342d7ced320a76de4945ef73a66d5fd4723
Hayden Young <hayden@hbjy.dev>
2022-11-24 21:40:34 +0000

    feat: make it work :)

--- examples/my-example/go/main.go
+++ examples/my-example/go/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"github.com/kuraudo-io/pulumi-symbiosis/sdk/go/symbiosis"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-  "github.com/kuraudo-io/pulumi-symbiosis/sdk/go/symbiosis"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-    _, err := symbiosis.NewCluster(ctx, "example", &symbiosis.ClusterArgs{
-      Region: pulumi.String("germany-1"),
-    })
-    if err != nil {
-      return err
-    }
+		_, err := symbiosis.NewCluster(ctx, "example", &symbiosis.ClusterArgs{
+			Region: pulumi.String("germany-1"),
+		})
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 }

21a3b2c5b9b956e87fed16ac9be125b8c3f540ae
Hayden Young <hayden@hbjy.dev>
2022-11-26 14:44:43 +0000

    feat(example): implement node pool in example

---
+++ examples/cluster-example/go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/kuraudo-io/pulumi-symbiosis/sdk/go/symbiosis"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		cluster, err := symbiosis.NewCluster(ctx, "example", &symbiosis.ClusterArgs{
+			Region: pulumi.String("germany-1"),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = symbiosis.NewNodePool(ctx, "pool1", &symbiosis.NodePoolArgs{
+			Cluster:  cluster.Name,
+			NodeType: pulumi.String("general-1"),
+			Quantity: pulumi.Int(1),
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
